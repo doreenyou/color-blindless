2c556c386fce6b8afb1be0e6f31c2e7e32f484a0
youran.y <youran.y@alibaba-inc.com>
2021-03-31 21:11:53 +0800

    add table view

--- resources/mod/index.jsx
+++ resources/mod/index.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { getFilteredImage } from '../common/colorblind';
+import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table'
+import '../../src/SuperResponsiveTableStyle.css';
 // 可以使用 antd
 // import { Select, Radio } from 'antd';
 
@@ -41,8 +43,34 @@ const APP = (props) => {
     types.map(type => {
       return (
         <div className = 'container'>
-          <div>{type}</div>
-          { colorBlindnessImgs[type] && <img src={colorBlindnessImgs[type]} /> }
+          <table key={i}>
+            <thead>
+              <tr>
+                <Th>{type[0]}</Th>
+                <Th>{type[1]}</Th>
+                <Th>{type[2]}</Th>
+                <Th>{type[3]}</Th>
+                <Th>{type[4]}</Th>
+                <Th>{type[5]}</Th>
+                <Th>{type[6]}</Th>
+                <Th>{type[7]}</Th>
+                <Th>{type[8]}</Th>
+              </tr>
+            </thead>
+            <Tbody>
+              <Tr>
+                <Td>{ colorBlindnessImgs[type[0]] && <img src={colorBlindnessImgs[type[0]]} /> }</Td>
+                <Td>{ colorBlindnessImgs[type[1]] && <img src={colorBlindnessImgs[type[1]]} /> }</Td>
+                <Td>{ colorBlindnessImgs[type[2]] && <img src={colorBlindnessImgs[type[2]]} /> }</Td>
+                <Td>{ colorBlindnessImgs[type[3]] && <img src={colorBlindnessImgs[type[3]]} /> }</Td>
+                <Td>{ colorBlindnessImgs[type[4]] && <img src={colorBlindnessImgs[type[4]]} /> }</Td>
+                <Td>{ colorBlindnessImgs[type[5]] && <img src={colorBlindnessImgs[type[5]]} /> }</Td>
+                <Td>{ colorBlindnessImgs[type[6]] && <img src={colorBlindnessImgs[type[6]]} /> }</Td>
+                <Td>{ colorBlindnessImgs[type[7]] && <img src={colorBlindnessImgs[type[7]]} /> }</Td>
+                <Td>{ colorBlindnessImgs[type[8]] && <img src={colorBlindnessImgs[type[8]]} /> }</Td>
+              </Tr>
+            </Tbody>
+          </Table>
          </div>
       )
     })
